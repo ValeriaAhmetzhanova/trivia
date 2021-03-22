7fd3ce9c79de2d65a94976b3c18e7e06375b2128
Valeria <v.ahmetzhanova@gmail.com>
2021-03-22 21:49:45 +0800

    add footer text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-bottom-navigation absolute class="footer"
-    >This is FOOTER placeholder</v-bottom-navigation
-  >
+  <div class="footer">
+    <div class="footer-text">
+      <h2>Get started & join with trivia for benefits</h2>
+      <h5>
+        Casual updates on trivia features, resume best practices, and
+        job-landing techniques.
+      </h5>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,9 +16,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 .footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
   background: transparent;
   box-shadow: none;
+  padding: 1rem 4rem;
+  .footer-text {
+    width: 50%;
+  }
 }
 </style>
