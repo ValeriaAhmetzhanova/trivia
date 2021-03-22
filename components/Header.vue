0f5c47e7223e5a5e9dc8448b641c7d03f1746d17
Valeria <v.ahmetzhanova@gmail.com>
2021-03-22 19:09:38 +0800

    style header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>This is HEADER placeholder</div>
+  <v-app-bar color="transparent" flat absolute elevate-on-scroll class="header">
+    <v-toolbar-title class="header-item">Trivia</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <nuxt-link class="header-item" to="/"><div>Home</div></nuxt-link>
+    <nuxt-link class="header-item" to="/"><div>Our Product</div></nuxt-link>
+    <nuxt-link class="header-item" to="/"><div>Pricing</div></nuxt-link>
+    <nuxt-link class="header-item" to="/"><div>Contact</div></nuxt-link>
+  </v-app-bar>
 </template>
 
 <script>
@@ -8,4 +17,16 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.header {
+  padding: 1rem 4rem;
+  .header-item {
+    text-decoration: none;
+    margin-right: 3rem;
+    color: var(--base-color);
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
